dfa04d8ca809e4308edb00b18feec4a27650f81c
PinhasZiv <63521561+PinhasZiv@users.noreply.github.com>
2022-10-13 21:43:41 +0300

    Delete Constants.py
    delete the file because it store the access data for the DB and the bot-token.
    need to add it somehow to gitignore

--- Constants.py
+++
@@ -1,6 +0,0 @@
-BOT_API_TOKEN = "5636457123:AAHI-QUumpjblVKgevzLVU71ftpydznLbmY"
-
-MYSQL_DB = {'host': 'localhost',
-            'database': 'distribution',
-            'user': 'root',
-            'password': 'Pziv8089869'}
